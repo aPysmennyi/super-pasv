566bf3915ec09d1d0a9ec2d554d209ace2986483
aPysmennyi <andreas170590@gmail.com>
2022-02-11 17:33:18 -0800

    add negative test

--- specs/auth.specs.js
+++ specs/auth.specs.js
@@ -3,14 +3,24 @@ import { expect } from 'chai'
 import 'dotenv/config'
 
 describe('Auth',function () {
+    const request = supertest(process.env.BASE_URL)
+
     it('Succesful login',function () {
-      const request = supertest(process.env.BASE_URL)
         request
             .post('/auth')
             .send({login: process.env.LOGIN, password: process.env.PASSWORD})
             .end(function (err,res) {
                 expect(res.statusCode).to.eq(200)
                 expect(res.body.token).not.to.be.undefined
+            })
+    })
+    it('Login with invalid credentials',function () {
+        request
+            .post('/auth')
+            .send({login: 'invalid', password: 'invalid'})
+            .end(function (err,res) {
+                expect(res.statusCode).to.eq(404)
+                expect(res.body.message).to.eq('Wrong login or password.')
             })
     })
 })
